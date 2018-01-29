eba0c36b401646f0628d05c3ff02b31774e96b5a
nongp <nongp@hotmail.com>
2018-01-29 17:38:47 +0700

    2nd push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -68,6 +68,12 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_cable.url = "ws://localhost:3000/cable"
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
@@ -91,6 +97,7 @@ Rails.application.configure do
        secret_access_key: 'nrSvvzuUvuQg8mjcde4J26wAUuP3gCiZqlc5GY7I',
        s3_region: 'ap-southeast-1'
      }
+    }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
