15f375b09f85552cb613874ac8ae3a1f05c9d294
nongp <nongp@hotmail.com>
2018-02-09 23:07:06 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
- config.action_cable.allowed_request_origins = [ 'http://www.staynplay.club', 'https://www.staynplay.club' ]
+ config.action_cable.allowed_request_origins = [ 'http://www.staynplay.club', 'https://www.staynplay.club', 'https://murmuring-bastion-34979.herokuapp.com/', 'http://murmuring-bastion-34979.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
