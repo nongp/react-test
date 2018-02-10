cde01f9e38ce94e8e0f67beeac2327377972b259
nongp <nongp@hotmail.com>
2018-02-11 05:27:36 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
- config.action_cable.url = 'wss://www.staynplay.club/cable'
+  #config.action_cable.url = 'wss://www.staynplay.club/cable'
  config.action_cable.allowed_request_origins = [ 'http://www.staynplay.club', 'https://www.staynplay.club' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
