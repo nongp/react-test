f324cf03e5588d8d6c08e924ce616e8288f9c4a7
nongp <nongp@hotmail.com>
2018-02-11 05:12:22 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+ config.action_cable.url = 'wss://www.staynplay.club/cable'
  config.action_cable.allowed_request_origins = [ 'http://www.staynplay.club', 'https://www.staynplay.club' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -58,7 +58,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "AirPikachu_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
