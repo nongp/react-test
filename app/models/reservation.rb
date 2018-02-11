fd165ad7175a6e7576b870995a28c69de9741e09
nongp <nongp@hotmail.com>
2018-02-11 07:51:58 +0700

    commit

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -18,6 +18,6 @@ class Reservation < ApplicationRecord
       type = self.room.Instant? ? "มี Book ใหม่!" : "มี Request ใหม่!"
       guest = User.find(self.user_id)
 
-      Notification.create(content: "#{type} จาก #{guest.fullname} ดูรายละเอียดที่'Guest Arrival'", user_id: self.room.user_id)
+      Notification.create(content: "#{type} จาก #{guest.fullname}", user_id: self.room.user_id)
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -98,7 +98,7 @@ Rails.application.configure do
        bucket: ENV['AWS_S3_BUCKET'],
        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-       s3_region: ENV['AWS_S3_BUCKET']
+       s3_region: 'ap-southeast-1'
      }
    }
 
