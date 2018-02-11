ed4475e753ba6cb72d156303070ef9591ce4cd0d
nongp <nongp@hotmail.com>
2018-02-11 11:24:46 +0700

    commit

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 # config/initializers/paperclip.rb
 
-Paperclip::Attachment.default_options[:s3_host_name] = 'https://staynplayclub.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:url] = ':class/:attachment/:id/:style/:filename'
+Paperclip::Attachment.default_options[:url] = 'staynplayclub.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.ap-southeast-1.amazonaws.com'
