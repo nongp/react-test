25fe6b136fdac77d3cb8bcdade2f46e163a4351d
nongp <nongp@hotmail.com>
2018-02-11 10:41:47 +0700

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,9 @@ Rails.application.configure do
   :bucket => ENV['AWS_BUCKET'],
   :s3_credentials => {    
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: ENV['AWS_S3_REGION']
+
   }
 }
 
