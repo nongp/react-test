25f4f0935fc779487798cb96501b9ca0bcc6b60e
nongp <nongp@hotmail.com>
2018-02-23 23:19:53 +0700

    aws setup in both production & development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,13 @@ Rails.application.configure do
 
     config.paperclip_defaults = {
   :storage => :s3,
-  :bucket => ENV['AWS_BUCKET'],
-  :s3_host_name => 's3.ap-southeast-1.amazonaws.com',
-  :s3_credentials => {    
+  :s3_region => ENV['AWS_S3_REGION'],
+  :s3_host_name => ENV['S3_HOST_NAME'],
+  :s3_credentials => { 
+    :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_region => ENV['AWS_S3_REGION']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    
 
   }
 }
