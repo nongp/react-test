1d09d7717dd372acd73065ce555cd85cc497d0d2
nongp <nongp@hotmail.com>
2018-02-24 19:17:03 +0700

    aws setup in both production & development

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,7 +6,8 @@ class Photo < ApplicationRecord
   	thumb: "100x100>" },
   	:storage => :s3,
   	:s3_credentials => "#{Rails.root}/config/aws.yml",
-  	:bucket => "staynplayclub"
+  	:bucket => "staynplayclub",
+  	:path => '/:class/:attachment/:id/:style/:filename'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
