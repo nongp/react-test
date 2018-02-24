32147a435d70c20614b82e3e2b2e2dfb5d28d4c5
nongp <nongp@hotmail.com>
2018-02-24 22:00:50 +0700

    aws setup in both production & development

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
 
   has_attached_file :image, styles: { 
   	medium: "300x300>", 
-  	thumb: "100x100>" }#,
+  	thumb: "100x100>" },
  # 	:storage => :s3,
  # 	:s3_credentials => "#{Rails.root}/config/aws.yml",
  # 	:bucket => "staynplayclub",
