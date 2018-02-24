234d14e5390569b47b1fbb9f57a6fea66bd986d3
nongp <nongp@hotmail.com>
2018-02-24 19:26:13 +0700

    aws setup in both production & development

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,11 +3,11 @@ class Photo < ApplicationRecord
 
   has_attached_file :image, styles: { 
   	medium: "300x300>", 
-  	thumb: "100x100>" },
-  	:storage => :s3,
-  	:s3_credentials => "#{Rails.root}/config/aws.yml",
-  	:bucket => "staynplayclub",
-  	:path => '/:class/:attachment/:id/:style/:filename'
+  	thumb: "100x100>" }#,
+ # 	:storage => :s3,
+ # 	:s3_credentials => "#{Rails.root}/config/aws.yml",
+ # 	:bucket => "staynplayclub",
+ # 	:path => '/:class/:attachment/:id/:style/:filename'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
