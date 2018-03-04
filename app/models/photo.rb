aef106f7d87fbc20fadff765a0cf54873494a361
nongp <nongp@hotmail.com>
2018-03-04 15:51:54 +0700

    snp-masterv5

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,15 +1,15 @@
 class Photo < ApplicationRecord
   belongs_to :room
 
-  #has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+ has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 
- has_attached_file :image, styles: { 
- 	 medium: "300x300>", 
- 	 thumb: "100x100>" },
- 	 :storage => :s3,
- 	 :s3_credentials => "#{Rails.root}/config/aws.yml",
- 	 :bucket => "snpdevelopment",
- 	 :path => '/:class/:attachment/:id/:style/:filename'
+ # has_attached_file :image, styles: { 
+ # 	 medium: "300x300>", 
+ # 	 thumb: "100x100>" },
+ # 	 :storage => :s3,
+ # 	 :s3_credentials => "#{Rails.root}/config/aws.yml",
+ # 	 :bucket => "snpdevelopment",
+ # 	 :path => '/:class/:attachment/:id/:style/:filename'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
