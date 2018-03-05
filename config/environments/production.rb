3cc2673026ecfa7486ca548f32b3cdc532f63eaf
nongp <nongp@hotmail.com>
2018-03-05 08:09:45 +0700

    rassetPrecompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.serve_static_assets = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   config.assets.initialize_on_precompile = true
